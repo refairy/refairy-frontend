d38962d237259c7e5be247d762cac58c92858f9a
jominjun <mj3029327@naver.com>
2020-11-27 10:14:01 +0900

    Graph 추가

--- scripts/index/index.js
+++ scripts/index/index.js
@@ -8,12 +8,30 @@ Array.from(document.getElementsByClassName("SearchHistory")).forEach(function (
   item
 ) {
   item.addEventListener("click", () => {
-    window.location.href = "./result";
+    window.location.href = "./report";
   });
 });
 
 const searchButton = document.getElementById("searchButton");
 
 searchButton.addEventListener("click", () => {
-  window.location.href = "./result";
+  window.location.href = "./report";
 });
+
+window.onload = () => {
+  let response = fetch(
+    "https://webbackend-ffwfi5ynba-uc.a.run.app/api/report/recents",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+};
